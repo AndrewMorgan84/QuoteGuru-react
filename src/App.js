ac8dae0b7d3aa5386bdd6cbda71420977da8c5ab
Andrew <andrew.morgan@e.org>
2022-02-16 20:35:38 +0000

    Added and implemented Layout component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" component={AllQuotes} exact />
-      <Route path="/quotes/:quoteId" component={QuoteDetail} />
-      <Route path="/new-quote" component={NewQuote} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" component={AllQuotes} exact />
+        <Route path="/quotes/:quoteId" component={QuoteDetail} />
+        <Route path="/new-quote" component={NewQuote} />
+      </Switch>
+    </Layout>
   );
 }
 
