7eee1bdb0f90cdf902d7f80ebce787a4cc21c751
Andrew <andrew.morgan@e.org>
2022-02-16 20:12:51 +0000

    Added pages and primitive routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" component={AllQuotes} exact />
+      <Route path="/quotes/:quoteId" component={QuoteDetail} />
+      <Route path="/new-quote" component={NewQuote} />
+    </Switch>
   );
 }
 
---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,9 @@
+const AllQuotes = () => {
+  return (
+    <div>
+      <h1>Quotes Page</h1>
+    </div>
+  );
+};
+
+export default AllQuotes;
